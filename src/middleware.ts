99d8f165244ec65c419310a7a1447559268eb0a0
Jay Kerkar <bytewise0405@gmail.com>
2025-02-15 19:04:05 +0000

    Updated Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import type { auth } from "@/lib/auth";
+import { betterFetch } from "@better-fetch/fetch";
+import { NextResponse, type NextRequest } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+export default async function authMiddleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    `${process.env.BETTER_AUTH_URL}/api/auth/get-session`,
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "",
+      },
+    }
+  );
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/:path*"],
+};
