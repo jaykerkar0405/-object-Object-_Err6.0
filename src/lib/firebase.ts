1c8d09d63effda4eae2022008d40d8388d42e8ca
Jay Kerkar <bytewise0405@gmail.com>
2025-02-15 16:03:38 +0000

    Added Push Notifications

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,38 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getMessaging, getToken, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  projectId: "yogasense-87631",
+  measurementId: "G-0T28X1J9Y7",
+  messagingSenderId: "227827215784",
+  authDomain: "yogasense-87631.firebaseapp.com",
+  apiKey: "AIzaSyCPkbsJjBtnSBi74u1J9m8kJPGOC4zOfnc",
+  appId: "1:227827215784:web:0a53fd461109537e2c819d",
+  storageBucket: "yogasense-87631.firebasestorage.app",
 };
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-export { messaging, getToken, onMessage };
+const messaging = async () => {
+  const supported = await isSupported();
+  return supported ? getMessaging(app) : null;
+};
+
+export const fetchToken = async () => {
+  try {
+    const fcmMessaging = await messaging();
+    if (fcmMessaging) {
+      const token = await getToken(fcmMessaging, {
+        vapidKey:
+          "BMZrxcRklCtGi9ecfNkbUqmC3bp8gpU2yj5cXE9owZlZcVraNO3utO_7LKavcCAD3dyvFIplIV_CPuVOS9rUisM",
+      });
+      return token;
+    }
+    return null;
+  } catch (err) {
+    console.error("An error occurred while fetching the token:", err);
+    return null;
+  }
+};
+
+export { app, messaging };
