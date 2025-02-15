7aa18b4e043ccede9822e1f3400d4a1e20a40921
zelli17 <aayushnair321@gmail.com>
2025-02-15 15:06:56 +0530

    cs3

--- src/lib/yoga-poses.ts
+++ src/lib/yoga-poses.ts
@@ -47,8 +47,8 @@ export const yogaPoses: YogaPose[] = [
     title: "Cobra Pose",
     name: "cobra-pose",
     constraints: (landmarks) => [
-      getJointFeedback(landmarks, "left-hip", { max: 160, min: 90 }),
-      getJointFeedback(landmarks, "right-hip", { max: 160, min: 90 }),
+      getJointFeedback(landmarks, "left-hip", { max: 140, min: 90 }),
+      getJointFeedback(landmarks, "right-hip", { max: 140, min: 90 }),
       getJointFeedback(landmarks, "left-elbow", { min: 160 }),
       getJointFeedback(landmarks, "right-elbow", { min: 140 }),
       getJointFeedback(landmarks, "left-knee", { min: 120 }),
