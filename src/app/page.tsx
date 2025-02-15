506eb64fd6ce25ed8339a53db9695b923e61c8bc
Jay Kerkar <bytewise0405@gmail.com>
2025-02-15 13:26:40 +0000

    Updated Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Component from "./LandingPage";
+import Link from "next/link";
+import Image from "next/image";
+import { IconCloudDemo } from "./Cloud";
+import { Button } from "@/components/ui/button";
+import { TypographyP } from "@/components/typography/P";
+import { TypographyH1 } from "@/components/typography/H1";
 
-export default function Home() {
+export default function Root() {
   return (
-    <div>
-      <Component />
-    </div>
+    <>
+      <div className="w-full flex justify-end pt-5 pr-5">
+        <Link href="/dashboard">
+          <Button>Get Started</Button>
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center -mt-10">
+        <Image
+          src="/favicon.png"
+          height={300}
+          width={300}
+          alt="YogaSense Icon"
+          className="mx-auto"
+        />
+        <div className="text-center">
+          <TypographyH1>YogaSense</TypographyH1>
+        </div>
+        <br />
+        <div className="text-center">
+          <TypographyP>AI Based Yoga Assistant</TypographyP>
+        </div>
+        <IconCloudDemo />
+      </div>
+    </>
   );
 }
